9858e7d759ce89916739872b1d01c6a70867dce7
Seu Nome <seu@email.com>
2025-10-23 18:46:49 -0300

    Removendo arquivos decadores e tempCodeRunner

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def mensagem_boas_vindas(nome):
-    print(f"Seja Bem-vindo , onde só os fortes sobrevivem {nome}!")
-    return f"Oi {nome}, você está pronto para começar?"
-
-executar = mensagem_boas_vindas("Arthur")
-executar = mensagem_boas_vindas("Beatriz")
